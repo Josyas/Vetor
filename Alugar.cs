30afc0edc6368dc525adfcc0d2d060283a12d7a5
Josias <87546094+Josyas@users.noreply.github.com>
2021-10-17 10:22:54 -0300

    Add files via upload

---
+++ Alugar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace vetor_aluguel {
+    class Alugar {
+
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        
+
+        public Alugar(string nome, string email) {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString() {
+            return "Quarto: " 
+                + "Nome: " 
+                + Nome 
+                + "E-mail: " 
+                + Email;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace vetor_aluguel {
+    class Program {
+        static void Main(string[] args) {
+
+            Alugar[] vect = new Alugar[10];
+
+            Console.WriteLine("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++) {
+
+                Console.WriteLine();
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("E-mail: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto ");
+                int quarto = int.Parse(Console.ReadLine());
+                vect[quarto] = new Alugar(nome, email);
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados: ");
+            for (int i = 0; i < 10; i++) {
+                if (vect[i] != null) {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+        }      
+        
+    }
+}
